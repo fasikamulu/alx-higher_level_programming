fa58c95055c762f17667c402fa82897871bfcc1f
fasikamulu <fasikamulugetak@gmail.com>
2024-02-23 14:08:11 +0300

    2-args modified

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
+    """Print the number of and list of arguments."""
     import sys
-    if (len(sys.argv) == 1):
+
+    count = len(sys.argv) - 1
+    if count == 0:
         print("0 arguments.")
-    elif (len(sys.argv) == 2):
+    elif count == 1:
         print("1 argument:")
-        print("1: {}".format(sys.argv[1]))
     else:
-        print("{} arguments:".format(len(sys.argv) - 1))
-        for i in range(1, len(sys.argv)):
-            print("{}: {}".format(i, sys.argv[i]))
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+
